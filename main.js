3fb2c3e72c499827066abc64cdb42b1ab5f28cba
Kyle <kyle.tz.cui@gmail.com>
2023-06-20 12:27:26 -0700

    unbroke

--- main.js
+++ main.js
@@ -5,32 +5,28 @@ import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 // Create a scene
 const scene = new THREE.Scene();
 // Create a renderer
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize(document.getElementById('canvas-container').offsetWidth, document.getElementById('canvas-container').offsetHeight);
-document.getElementById('canvas-container').appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer({antialias: true, canvas: document.getElementById("canv")});
+renderer.setSize(window.innerWidth, window.innerHeight);
 // Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-//Debug camera position
-//camera.position.set(0, 7, 5);
-//camera.rotateX(-Math.PI/3);
-const cameraXSpeed = 50;
-const cameraZSpeed = 1000;
-const cameraXOffset = 10;
-const cameraZOffset = 8;
-//camera.position.set(window.scrollY/cameraXSpeed + cameraXOffset, 7, window.scrollY/cameraZSpeed + cameraZOffset);
-camera.position.set(9, 4, window.scrollY/cameraZSpeed + cameraZOffset);
+const cameraZSpeed = 80;
+const cameraZOffset = 5;
+camera.position.set(5, 4, window.scrollY/cameraZSpeed + cameraZOffset);
 camera.rotateY(Math.PI/4);
 camera.rotateX(-Math.PI/4);
 // Create directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.9); // Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Directional light
 directionalLight.position.set(10, 10, 10); // Set the position of the light source
 scene.add(directionalLight);
+// Create grid helper
+const gridHelper = new THREE.GridHelper(100, 100);
+//scene.add(gridHelper);
 
 // Create a cube
 const cubeGeo = new THREE.BoxGeometry(0.3, 0.3, 0.3);
-const cubeMat = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+const cubeMat = new THREE.MeshPhongMaterial({ color: 0xeb4034 });
 const cubeMesh = new THREE.Mesh(cubeGeo, cubeMat);
-cubeMesh.castShadow = true;
+cubeMesh.position.y = 0.3;
 cubeMesh.receiveShadow = true;
 scene.add(cubeMesh);
 
@@ -62,47 +58,45 @@ textCanvas.width = 350;
 textCanvas.height = 350;
 const textContext = textCanvas.getContext('2d');
 textContext.fillStyle = 'white';
-textContext.fillRect(0, 0, textCanvas.width, textCanvas.height);
+textContext.fillStyle = 'black';
+//textContext.fillRect(0, 0, textCanvas.width, textCanvas.height);
 textContext.font = '36px Arial'; // Set the font properties
 textContext.fillStyle = 'black'; // Set the text color
+textContext.fillStyle = 'white';
 textContext.fillText('Hello, world!', 0, 50);
 textContext.fillText('Let\'s see if new', 0, 100);
 const textTexture = new THREE.Texture(textCanvas);
 textTexture.needsUpdate = true;
 const textMat = new THREE.MeshBasicMaterial({ map: textTexture });
-
 const textMesh = new THREE.Mesh(planeGeo, textMat);
 textMesh.position.set(2.5, 0, 5);
 textMesh.rotation.set(-Math.PI/2, 0, 0);
 scene.add(textMesh);
 
+// Create blue cube for debugging
 const blueMat = new THREE.MeshBasicMaterial({ color: 0x4232a8 });
 const blueCubeMesh = new THREE.Mesh(cubeGeo, blueMat);
+blueCubeMesh.position.y = 0.3;
+blueCubeMesh.receiveShadow = true;
 
-const initScrollY = window.scrollY;
-console.log("init: " + initScrollY);
 // Listen for scrolling
 function onScroll() {
-	const currScrollY = window.scrollY;
-	console.log(currScrollY);
-	//console.log(camera.position);
 	scene.add(blueCubeMesh);
+	blueCubeMesh.position.z = window.scrollY/cameraZSpeed;
+	blueCubeMesh.rotation.x += 0.01;
+	blueCubeMesh.rotation.y += 0.01;
 
-	//camera.position.x = -window.scrollY/cameraXSpeed + cameraXOffset;
-	//console.log(window.scrollY/cameraZSpeed + cameraZOffset)
-	//camera.position.z = window.scrollY/cameraZSpeed + cameraZOffset;
+	camera.position.z = window.scrollY/cameraZSpeed + cameraZOffset;
 }
-document.addEventListener('scroll', onScroll);
+window.addEventListener('scroll', onScroll);
 
 // Render the scene
 function animate() {
-  requestAnimationFrame(animate);
-
-  cubeMesh.rotation.x += 0.01;
-  cubeMesh.rotation.y += 0.01;
-  //camera.position.z += 0.1;
-
-  renderer.render(scene, camera);
+	cubeMesh.rotation.x += 0.01;
+	cubeMesh.rotation.y += 0.01;
+	
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
 }
 
 animate();
